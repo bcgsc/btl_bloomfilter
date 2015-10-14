ee9518fe4e58bf69bf28058ea8b7750bab53b7ba
JustinChu <cjustin@bcgsc.ca>
2015-10-13 18:11:14 -0700

    code stubs for rolling hash

--- BloomFilter/BloomFilter.hpp
+++ BloomFilter/BloomFilter.hpp
@@ -28,23 +28,6 @@ static const uint8_t bitsPerChar = 0x08;
 static const unsigned char bitMask[0x08] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20,
 		0x40, 0x80 };
 
-//TODO Work out better way to deal with kmerSize since conversion to kmerSize in bytes is needed
-
-/*
- * For precomputing hash values. kmerSize is the number of bytes of the original string used.
- */
-static inline vector<size_t> multiHash(const unsigned char* kmer, size_t num,
-		unsigned kmerSize) {
-	vector < size_t > tempHashValues(num);
-	size_t kmerSizeInBytes = (kmerSize + 3) / 4;
-
-	for (size_t i = 0; i < num; ++i) {
-		tempHashValues[i] = CityHash64WithSeed(
-				reinterpret_cast<const char*>(kmer), kmerSizeInBytes, i);
-	}
-	return tempHashValues;
-}
-
 class BloomFilter {
 public:
 	/* De novo filter constructor.
@@ -54,10 +37,8 @@ public:
 	 * kmerSize refers to the number of bases the kmer has
 	 * k-mers supplied to this object should be binary (2 bits per base)
 	 */
-	BloomFilter(size_t filterSize, unsigned hashNum,
-			unsigned kmerSize) :
-			m_size(filterSize), m_hashNum(hashNum), m_kmerSize(kmerSize), m_kmerSizeInBytes(
-					(kmerSize + 4 - 1) / 4) {
+	BloomFilter(size_t filterSize, unsigned hashNum, unsigned kmerSize) :
+			m_size(filterSize), m_hashNum(hashNum), m_kmerSize(kmerSize) {
 		initSize(m_size);
 		memset(m_filter, 0, m_sizeInBytes);
 	}
@@ -65,10 +46,9 @@ public:
 	/*
 	 * Loads the filter (file is a .bf file) from path specified
 	 */
-	BloomFilter(size_t filterSize, unsigned hashNum,
-			unsigned kmerSize, string const &filterFilePath) :
-			m_size(filterSize), m_hashNum(hashNum), m_kmerSize(kmerSize), m_kmerSizeInBytes(
-					(kmerSize + 4 - 1) / 4) {
+	BloomFilter(size_t filterSize, unsigned hashNum, unsigned kmerSize,
+			string const &filterFilePath) :
+			m_size(filterSize), m_hashNum(hashNum), m_kmerSize(kmerSize) {
 		initSize(m_size);
 
 		FILE *file = fopen(filterFilePath.c_str(), "rb");
@@ -98,6 +78,29 @@ public:
 	}
 
 	/*
+	 * For precomputing hash values. kmerSize is the number of bytes of the original string used.
+	 */
+	//TODO: change to rolling hash
+	vector<size_t> multiHash(const unsigned char* kmer) {
+		vector<size_t> tempHashValues(m_hashNum);
+
+		for (size_t i = 0; i < m_hashNum; ++i) {
+			tempHashValues[i] = CityHash64WithSeed(
+					reinterpret_cast<const char*>(kmer), m_kmerSize, i);
+		}
+		return tempHashValues;
+	}
+
+	/*
+	 * For precomputing hash values. kmerSize is the number of bytes of the original string used.
+	 */
+	//TODO: For rolling hash
+	vector<size_t> multiHash(size_t previousValue, unsigned char in,
+			unsigned char out) {
+		return 0;
+	}
+
+	/*
 	 * Accepts a list of precomputed hash values. Faster than rehashing each time.
 	 */
 	void insert(vector<size_t> const &precomputed) {
@@ -112,17 +115,23 @@ public:
 		}
 	}
 
+	//TODO: change to rolling hash
 	void insert(const unsigned char* kmer) {
 		//iterates through hashed values adding it to the filter
 		for (size_t i = 0; i < m_hashNum; ++i) {
 			size_t normalizedValue = CityHash64WithSeed(
-					reinterpret_cast<const char*>(kmer), m_kmerSizeInBytes, i)
+					reinterpret_cast<const char*>(kmer), m_kmerSize, i)
 					% m_size;
 			__sync_or_and_fetch(&m_filter[normalizedValue / bitsPerChar],
 					bitMask[normalizedValue % bitsPerChar]);
 			//		m_filter[normalizedValue / bitsPerChar] |= bitMask[normalizedValue
 			//				% bitsPerChar];
 		}
+	}
+
+	//TODO: For rolling hash
+	void insert(const unsigned char* kmer, unsigned char in,
+			unsigned char out) {
 	}
 
 	/*
@@ -142,10 +151,11 @@ public:
 	/*
 	 * Single pass filtering, computes hash values on the fly
 	 */
+	//TODO: change to rolling hash
 	bool contains(const unsigned char* kmer) const {
 		for (unsigned i = 0; i < m_hashNum; ++i) {
 			size_t normalizedValue = CityHash64WithSeed(
-					reinterpret_cast<const char*>(kmer), m_kmerSizeInBytes, i)
+					reinterpret_cast<const char*>(kmer), m_kmerSize, i)
 					% m_size;
 			unsigned char bit = bitMask[normalizedValue % bitsPerChar];
 			if ((m_filter[normalizedValue / bitsPerChar] & bit) != bit) {
@@ -153,6 +163,15 @@ public:
 			}
 		}
 		return true;
+	}
+
+	/*
+	 * Single pass filtering, computes hash values on the fly
+	 */
+	//TODO: For rolling hash
+	bool contains(const unsigned char* kmer, unsigned char in,
+			unsigned char out) const {
+		return false;
 	}
 
 	/*
@@ -206,7 +225,6 @@ private:
 	size_t m_sizeInBytes;
 	unsigned m_hashNum;
 	unsigned m_kmerSize;
-	unsigned m_kmerSizeInBytes;
 };
 
 #endif /* BLOOMFILTER_H_ */
