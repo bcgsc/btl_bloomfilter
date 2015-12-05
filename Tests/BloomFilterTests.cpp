cb9eca5c210ad6cd0e1aa6999375633159ef7ffa
Ben Vandervalk <benv@bcgsc.ca>
2015-12-04 17:10:20 -0800

    BloomFilterTests: convert to unit tests
    Keep original version as `BloomFilterAdHocTests.cpp`, because it has
    some tests that can't be automated easily (e.g. memory usage checks that
    require manually examining the output).

--- Tests/BloomFilterTests.cpp
+++ Tests/BloomFilterTests.cpp
@@ -5,41 +5,36 @@
  *      Author: cjustin
  */
 
+/* automatically create main() function to run tests */
+#define CATCH_CONFIG_MAIN
+
+/* lightweight unit test framework */
+#include "catch.hpp"
 #include "BloomFilter.hpp"
+
 #include <string>
 #include <assert.h>
-#include <vector>
-#include <iostream>
 #include <fstream>
 #include <sstream>
-#if _OPENMP
-# include <omp.h>
-#endif
 
 using namespace std;
 
-//returns memory of program in kb
-unsigned memory_usage() {
-	unsigned mem = 0;
-	ifstream proc("/proc/self/status");
-	string s;
-	while (getline(proc, s), !proc.fail()) {
-		if (s.substr(0, 6) == "VmSize") {
-			stringstream convert(
-					s.substr(s.find_last_of('\t'), s.find_last_of('k') - 1));
-			if (!(convert >> mem)) {
-				return 0;
-			}
-			return mem;
-		}
-	}
-	return mem;
-}
+TEST_CASE("test fixture", "[BloomFilter]")
+{
+	/*
+	 * NOTES:
+	 * - The SECTION blocks below are separate tests that share the
+	 * same setup code
+	 * - The common setup code is _re-run_ before each SECTION
+	 * - In unit test terminology, this type of setup is known as a
+	 * "test fixture"
+	 * - See https://github.com/philsquared/Catch/blob/master/docs/tutorial.md#test-cases-and-sections
+	 * for details
+	 */
 
-int main() {
-	//memory usage from before
-	int memUsage = memory_usage();
+	/* START COMMON SETUP CODE */
 
+	//test Bloom filter with some elements
 	size_t filterSize = 1000000000;
 	BloomFilter filter(filterSize, 5, 20);
 	filter.insert("ATCGGGTCATCAACCAATAT");
@@ -47,98 +42,51 @@ int main() {
 	filter.insert("ATCGGGTCATCAACCAATAG");
 	filter.insert("ATCGGGTCATCAACCAATAA");
 
-	//Check if filter is able to report expected results
-	assert(filter.contains("ATCGGGTCATCAACCAATAT"));
-	assert(filter.contains("ATCGGGTCATCAACCAATAC"));
-	assert(filter.contains("ATCGGGTCATCAACCAATAG"));
-	assert(filter.contains("ATCGGGTCATCAACCAATAA"));
+	/* END COMMON SETUP CODE */
 
-	assert(!filter.contains("ATCGGGTCATCAACCAATTA"));
-	assert(!filter.contains("ATCGGGTCATCAACCAATTC"));
+	SECTION("query elements")
+	{
+		//Check if filter is able to report expected results
+		REQUIRE(filter.contains("ATCGGGTCATCAACCAATAT"));
+		REQUIRE(filter.contains("ATCGGGTCATCAACCAATAC"));
+		REQUIRE(filter.contains("ATCGGGTCATCAACCAATAG"));
+		REQUIRE(filter.contains("ATCGGGTCATCAACCAATAA"));
 
-	//should be size of bf (amortized)
-	cout << memory_usage() - memUsage << "kb" << endl;
-
-	cout << "de novo bf tests done" << endl;
-
-	//Check storage can occur properly
-	string filename = "/tmp/bloomFilter.bf";
-	filter.storeFilter(filename);
-	ifstream ifile(filename.c_str());
-	assert(ifile.is_open());
-	ifile.seekg(0, ios::end); // move to end of file
-	size_t fileSize = ifile.tellg(); // file size in bytes
-	//file size should be same as filter size (Round to block size)
-	if (filterSize % 64 > 0) {
-		assert((filterSize + (64 - (filterSize% 64))) == fileSize*8);
-	} else {
-		assert(filterSize == fileSize*8);
+		REQUIRE(!filter.contains("ATCGGGTCATCAACCAATTA"));
+		REQUIRE(!filter.contains("ATCGGGTCATCAACCAATTC"));
 	}
-	ifile.close();
 
-	//should be roughly same size still (amortized)
-	cout << memory_usage() - memUsage << "kb" << endl;
+	SECTION("save/load Bloom file")
+	{
+		//Check storage can occur properly
+		string filename = "/tmp/bloomFilter.bf";
+		filter.storeFilter(filename);
+		ifstream ifile(filename.c_str());
+		assert(ifile.is_open());
+		ifile.seekg(0, ios::end); // move to end of file
+		size_t fileSize = ifile.tellg(); // file size in bytes
+		//file size should be same as filter size (Round to block size)
+		if (filterSize % 64 > 0) {
+			assert((filterSize + (64 - (filterSize% 64))) == fileSize*8);
+		} else {
+			assert(filterSize == fileSize*8);
+		}
+		ifile.close();
 
-	//check loading of stored filter
-	BloomFilter filter2(filterSize, 5, 20, filename);
+		//check loading of stored filter
+		BloomFilter filter2(filterSize, 5, 20, filename);
 
-	//should be double size of bf (amortized)
-	cout << memory_usage() - memUsage << "kb" << endl;
+		//Check if loaded filter is able to report expected results
+		REQUIRE(filter2.contains("ATCGGGTCATCAACCAATAT"));
+		REQUIRE(filter2.contains("ATCGGGTCATCAACCAATAC"));
+		REQUIRE(filter2.contains("ATCGGGTCATCAACCAATAG"));
+		REQUIRE(filter2.contains("ATCGGGTCATCAACCAATAA"));
 
-	//Check if loaded filter is able to report expected results
-	assert(filter2.contains("ATCGGGTCATCAACCAATAT"));
-	assert(filter2.contains("ATCGGGTCATCAACCAATAC"));
-	assert(filter2.contains("ATCGGGTCATCAACCAATAG"));
-	assert(filter2.contains("ATCGGGTCATCAACCAATAA"));
+		REQUIRE(!filter2.contains("ATCGGGTCATCAACCAATTA"));
+		REQUIRE(!filter2.contains("ATCGGGTCATCAACCAATTC"));
 
-	assert(!filter2.contains("ATCGGGTCATCAACCAATTA"));
-	assert(!filter2.contains("ATCGGGTCATCAACCAATTC"));
-	cout << "premade bf tests done" << endl;
+		remove(filename.c_str());
+	}
 
-	//memory leak tests
-	BloomFilter* filter3 = new BloomFilter(filterSize, 5, 20);
-
-	size_t tempMem = memory_usage() - memUsage;
-
-	cout << memory_usage() - memUsage << "kb" << endl;
-	delete(filter3);
-	cout << memory_usage() - memUsage << "kb" << endl;
-	assert(tempMem != memory_usage() - memUsage);
-
-//	vector<vector<int> > test(1, vector<int>(1, 1));
-
-//	vector<BloomFilter> test;
-//	test.push_back(filter2);
-
-	cout << "memory leak prevention tests done" << endl;
-	cout << memory_usage() - memUsage << "kb" << endl;
-
-	remove(filename.c_str());
-
-//	//check parallelized code speed
-//	cout << "testing code parallelization" << endl;
-//	double start_s = omp_get_wtime();
-//
-//	cout << "parallelization" << endl;
-//	for( int i =0; i < 10000000; i++)
-//	{
-//		vector<size_t> values = multiHash("ATCGGGTCATCAACCAATAA", 5, 20);
-//		filter.contains(values);
-//	}
-//	double stop_s=omp_get_wtime();
-//	cout << "time: " << stop_s-start_s << endl;
-//
-//	start_s = omp_get_wtime();
-//	cout << "non parallelization" << endl;
-//	for( int i =0; i < 10000000; i++)
-//	{
-//		vector<size_t> values = multiHashNonPara("ATCGGGTCATCAACCAATAA", 5, 20);
-//		filter.contains(values);
-//	}
-//	stop_s=omp_get_wtime();
-//	cout << "time: " << stop_s-start_s << endl;
-
-	cout << "done" << endl;
-	return 0;
-}
+} /* end test fixture */
 
