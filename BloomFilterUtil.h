832e3953103b1a22167d53fd860109943a0bfb52
JustinChu <cjustin@bcgsc.ca>
2017-01-13 16:12:52 -0800

    adapt to use nthashiterator

--- BloomFilterUtil.h
+++ BloomFilterUtil.h
@@ -1,14 +1,13 @@
 #ifndef BLOOM_FILTER_UTIL
 #define BLOOM_FILTER_UTIL 1
 
-#include "RollingHash.h"
+#include "ntHashIterator.hpp"
 #include "BloomFilter.hpp"
-#include "RollingHashIterator.h"
 
 using namespace std;
 
 void insertSeq(BloomFilter &bloom, const string &seq, unsigned hashNum, unsigned kmerSize) {
-    RollingHashIterator itr(seq, hashNum, kmerSize); 
+	ntHashIterator itr(seq, hashNum, kmerSize);
     while (itr != itr.end()) {
         bloom.insert(*itr);
         itr++;
