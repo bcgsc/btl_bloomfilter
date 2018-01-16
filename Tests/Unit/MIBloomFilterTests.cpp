c1e911ba35eddf382c998aebc5175a58f3ba21be
JustinChu <cjustin@bcgsc.ca>
2018-01-15 16:56:29 -0800

    add some example code

--- Tests/Unit/MIBloomFilterTests.cpp
+++ Tests/Unit/MIBloomFilterTests.cpp
@@ -2,13 +2,14 @@
 #define CATCH_CONFIG_MAIN
 /* lightweight unit test framework */
 #include "catch.hpp"
-#include <vector>
+#include <sdsl/int_vector.hpp>
 
 using namespace std;
 
 #include "MIBloomFilter.hpp"
 
-TEST_CASE("TEST DESCRIPTION", "[MIBloomFilter]")
+TEST_CASE("TEST EXAMPLE", "[MIBloomFilter]")
 {
-
+	sdsl::bit_vector bv(1000);
+	cout << bv.size() << end;
 }
