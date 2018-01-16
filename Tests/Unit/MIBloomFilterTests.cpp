ac23bfb172c11a2b1d95797ddca1a4c308ad99e1
JustinChu <cjustin@bcgsc.ca>
2018-01-15 16:38:45 -0800

    add unit test files for MIBF

---
+++ Tests/Unit/MIBloomFilterTests.cpp
@@ -0,0 +1,14 @@
+/* automatically create main() function to run tests */
+#define CATCH_CONFIG_MAIN
+/* lightweight unit test framework */
+#include "catch.hpp"
+#include <vector>
+
+using namespace std;
+
+#include "MIBloomFilter.hpp"
+
+TEST_CASE("TEST DESCRIPTION", "[MIBloomFilter]")
+{
+
+}
