0aba0f623bca62ce8bae5a4843aaa0c33f3d533d
rup <spchowdhury@bcgsc.ca>
2018-11-01 17:45:14 -0700

    Modify insert() by adding a atomic compare-and-swap operation in high hopes of making it thread-safe. Rename a member variable m_countMax to m_countThreshold. Add more comments.

--- CountBloomFilter.hpp
+++ CountBloomFilter.hpp
@@ -1,46 +1,27 @@
 #ifndef COUNTBLOOM_H
 #define COUNTBLOOM_H
 
-#include <string>
 #include <cstring>
 #include <limits>
-#include <vector>
-#include <map>
-
-#if _OPENMP
-# include <omp.h>
-#endif
-
-using namespace std;
+#include <cmath>
 
 template <typename T>
 class CountBloomFilter {
 public:
-	CountBloomFilter(): m_filter(NULL), m_size(0), m_sizeInBytes(0), m_hashNum(0),
-			    m_kmerSize(0),  m_nEntry(0), m_countMax(0) { }
-	CountBloomFilter(size_t sz, unsigned m_hashNum, unsigned m_kmerSize, int m_countMax)
+	CountBloomFilter(size_t sz, unsigned m_hashNum, unsigned m_kmerSize, int m_countThreshold)
 		: m_hashNum(m_hashNum), m_kmerSize(m_kmerSize), m_nEntry(0),
-		  m_countMax(m_countMax)  {
-		// Round up to a multiple of 64 and initialize filter array.
+		  m_countThreshold(m_countThreshold)  {
+		// Round up sz to a multiple of 64.
 		m_size = ((sz - 1) | 63) + 1;
 		m_filter  = new T[m_size];
 		std::memset(m_filter, 0, sizeof(T) * m_size);
 		m_sizeInBytes = m_size * sizeof(T);
-
-#ifdef _OPENMP
-		m_locks.resize(10000);
-		for (size_t i = 0; i < m_locks.size(); ++i)
-			omp_init_nest_lock(&(m_locks.at(i)));
-#endif
 	}
 	~CountBloomFilter() {
 		delete[] m_filter;
-#ifdef _OPENMP
-		for (size_t i = 0; i < m_locks.size(); ++i)
-			omp_destroy_nest_lock(&(m_locks.at(i)));
-#endif
 	}
-	T minCount(const size_t hashes[]) const;
+	template <typename U>
+	T minCount(const U &hashes) const;
 	template <typename U>
 	bool contains(const U &hashes) const;
 	template <typename U>
@@ -48,87 +29,83 @@ public:
 	unsigned getKmerSize(void) const;
 	unsigned getHashNum(void) const;
 	size_t getFilterSize() const;
-#ifdef _OPENMP
-	void acquireLocks(const size_t hashes[]);
-	void releaseLocks(const size_t hashes[]);
-#endif
 	size_t popCount() const;
 	double FPR(void) const;
 
-	// STUBS:
-	CountBloomFilter(const string& path);
-	void loadFilter(const string&);
-	friend std::ostream& operator<<(std::ostream& out,
-					const CountBloomFilter& bloom) {
-		assert(out);
-		// bloom.writeHeader(out);
-		assert(out);
-		out.write(reinterpret_cast<char*>(bloom.m_filter), bloom.m_sizeInBytes);
-		assert(out);
-		return out;
-	}
-	// STUBS.
-
-	map<string, uint64_t> htab; // To ever compare bloom counts with true counts.
-
 private:
-#ifdef _OPENMP
-	bool acquireLockNonBlocking(size_t hashVal);
-	void acquireLock(size_t hashVal);
-	void releaseLock(size_t hashVal);
-	omp_nest_lock_t& getLock(size_t hashVal);
+	// m_filter         : A array of elements of type T; the bit-array or filter.
+	// m_size           : Size of bloom filter (number of counters in m_filter).
+	// m_sizeInBytes    : Size of the bloom filter in bytes (m_size * sizeof(T)).
+	// m_hashNum        : Number of hash functions.
+	// m_kmerSize       : Size of a k-mer.
+	// m_nEntry         : Number of items the bloom filter holds.
+	// m_countThreshold : A count greater or equal to this threshold
+	//                    establishes existence of an element in the filter.
 
-	mutable std::vector<omp_nest_lock_t> m_locks;
-	T        *m_filter;     // A array of elements of type T; the bit-array or filter.
-	size_t   m_size;        // Size of bloom filter (number of counters in m_filter).
-	size_t   m_sizeInBytes; // Size of the bloom filter in bytes (m_size * sizeof(T)).
-	unsigned m_hashNum;     // Number of hash functions.
-	unsigned m_kmerSize;    // Length of a k-mer.
-	size_t   m_nEntry;      // Number of items the bloom filter holds.
-	T        m_countMax;    // Maximum value a T can hold; use to check for overflow.
-#endif
+	T        *m_filter;        
+	size_t   m_size;           
+	size_t   m_sizeInBytes;    
+	unsigned m_hashNum;        
+	unsigned m_kmerSize;       
+	size_t   m_nEntry;         
+	T        m_countThreshold; 
 };
 
 // Method definitions.
-	
+
+/*  Return the minimum count of the m_hashNum positions of m_filter. */
 template <typename T>
-T CountBloomFilter<T>::minCount(const size_t hashes[]) const {
+template <typename U>
+T CountBloomFilter<T>::minCount(const U &hashes) const {
 	T min = m_filter[hashes[0] % m_size];
 	for (size_t i = 1; i < m_hashNum; ++i) {
-		size_t bit = hashes[i] % m_size;
-#ifdef _OPENMP
-		acquireLock(hashes[i]);
-		if (m_filter[bit] < min)
-			min = m_filter[bit];
-		releaseLock(hashes[i]);
-#else
-		if (m_filter[bit] < min)
-			min = m_filter[bit];
-#endif
+		size_t pos = hashes[i] % m_size;
+		if (m_filter[pos] < min)
+			min = m_filter[pos];
 	}
 	return min;
 }
 
+/*
+Check if an element exists. If the minimum count at the m_hashNum positions of
+m_filter is more than or equal to a predefined count threshold, then the element
+is said to be present in the Bloom filter. count() therefore returns true when
+this condition is satisfied, or else, false.
+*/
 template <typename T>
 template <typename U>
 bool CountBloomFilter<T>::contains(const U &hashes) const {
-	return minCount(hashes) >= m_countMax;
+	return minCount(hashes) >= m_countThreshold;
 }
 
+/*
+Increment all the m_hashNum positions of the m_filter array by one.
+
+A atomic compare-and-swap (CAS) operation increments m_filter[pos]. The CAS
+operation takes a memory location and a value that the caller believes the
+location currently stores. If the memory location still holds that value when
+the atomic compare-and-swap executes, then a new value is stored and 'true'
+returned; otherwise, memory is left unchanged and 'false' returned.
+
+The value of m_filter[pos] may be changed by another thread between a read from
+that memory location and a write to it. The CAS operation is called in a loop
+until it succeeds, which ensures that a write does not happen if some other
+thread has incremented the value after between this thread's read and write.
+
+Note that CAS operations suffer from the ABA problem.
+*/
 template <typename T>
 template <typename U>
 void CountBloomFilter<T>::insert(const U &hashes) {
+	static T currentVal, newVal;
 	for (size_t i = 0; i < m_hashNum; ++i) {
-		size_t bit = hashes[i] % m_size;
-#ifdef _OPENMP
-		acquireLock(hashes[i]);
-		if (m_filter[bit] < std::numeric_limits<T>::max())
-			m_filter[bit]++;
-		releaseLock(hashes[i]);
-#else
-		if (m_filter[bit] < std::numeric_limits<T>::max())
-			m_filter[bit]++;
-#endif
+		size_t pos = hashes[i] % m_size;
+		do {
+			currentVal = m_filter[pos];
+			newVal     = currentVal + 1;
+			if (newVal < currentVal)
+				break;
+		} while(!__sync_bool_compare_and_swap(&m_filter[pos], currentVal, newVal));
 	}
 }
 
@@ -141,56 +118,7 @@ unsigned CountBloomFilter<T>::getHashNum(void) const { return m_hashNum; }
 template <typename T>
 size_t CountBloomFilter<T>::getFilterSize() const { return m_size; }
 
-
-// Utilities
-
-#ifdef _OPENMP
-// Acquire all the locks, or else, quit.
-template <typename T>
-void CountBloomFilter<T>::acquireLocks(const size_t hashes[]) {
-	bool acquiredAllLocks = false;
-	while (!acquiredAllLocks) {
-		acquiredAllLocks = true;
-		for (size_t i = 0; i < m_hashNum; ++i) {
-			if (!acquireLockNonBlocking(hashes[i])) {
-				acquiredAllLocks = false;
-				for (size_t j = 0; j < i; ++j)
-					releaseLock(hashes[j]);
-				break;
-			}
-		}
-	}
-}
-
-template <typename T>
-void CountBloomFilter<T>::releaseLocks(const size_t hashes[]) {
-	for (size_t i = 0; i < m_hashNum; ++i)
-		releaseLock(hashes[i]);
-}
-
-template <typename T>
-bool CountBloomFilter<T>::acquireLockNonBlocking(size_t hashVal) {
-	return omp_test_nest_lock(&getLock(hashVal));
-}
-
-template <typename T>
-void CountBloomFilter<T>::acquireLock(size_t hashVal) {
-	omp_set_nest_lock(&getLock(hashVal));
-}
-
-template <typename T>
-void CountBloomFilter<T>::releaseLock(size_t hashVal) {
-	omp_unset_nest_lock(&getLock(hashVal));
-}
-
-template <typename T>
-omp_nest_lock_t& CountBloomFilter<T>::getLock(size_t hashVal) {
-	size_t lockIndex = (hashVal % m_size) % m_locks.size();
-	return m_locks.at(lockIndex);
-}
-
-#endif // _OPENMP
-
+/* Count the number of non-zero counters. */
 template <typename T>
 size_t CountBloomFilter<T>::popCount() const {
 	size_t count = 0;
@@ -203,7 +131,7 @@ size_t CountBloomFilter<T>::popCount() const {
 
 template <typename T>
 double CountBloomFilter<T>::FPR(void) const {
-	return pow((double)popCount() / (double)m_size, m_hashNum);
+	return std::pow((double)popCount() / (double)m_size, m_hashNum);
 }
 
 #endif // COUNTBLOOM_H
