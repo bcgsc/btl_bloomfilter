04f341dc42f840a8f9bc85a9a8bbc793127e97d0
Johnathan Wong <jowong@jowong05.phage.bcgsc.ca>
2019-06-27 11:00:26 -0700

    Revert "Removed unused variable"
    This reverts commit 9dfcdc19308bc0137c2c1fa9d659212c7ae74b54.

--- CountingBloomFilter.hpp
+++ CountingBloomFilter.hpp
@@ -29,6 +29,9 @@ class CountingBloomFilter
 	  , m_sizeInBytes(0)
 	  , m_hashNum(0)
 	  , m_kmerSize(0)
+	  , m_dFPR(0)
+	  , m_nEntry(0)
+	  , m_tEntry(0)
 	  , m_countThreshold(0)
 	{}
 	CountingBloomFilter(size_t sz, unsigned hashNum, unsigned kmerSize, unsigned countThreshold)
@@ -37,6 +40,9 @@ class CountingBloomFilter
 	  , m_sizeInBytes(sz * sizeof(T))
 	  , m_hashNum(hashNum)
 	  , m_kmerSize(kmerSize)
+	  , m_dFPR(0)
+	  , m_nEntry(0)
+      , m_tEntry(0)
 	  , m_countThreshold(countThreshold)
 	{
 		std::memset(m_filter, 0, m_sizeInBytes);
@@ -83,6 +89,9 @@ class CountingBloomFilter
 		uint64_t size;
 		uint32_t nhash;
 		uint32_t kmer;
+		double dFPR; //unused
+		uint64_t nEntry; //unused
+		uint64_t tEntry; //unsed
 		uint32_t version;
 		uint32_t bitsPerCounter;
 	};
@@ -100,6 +109,9 @@ class CountingBloomFilter
 	//                        (m_size * sizeof(T)).
 	// m_hashNum            : Number of hash functions.
 	// m_kmerSize           : Size of a k-mer.
+	// m_dFPR               : Unused variable kept for compatability reasons.
+	// m_nEntry             : Unused variable kept for compatability reasons.
+	// m_tEntry             : Unused variable kept for compatability reasons.
 	// BloomFilter_VERSION  : Size of a k-mer.
 	// m_countThreshold     : A count greater or equal to this threshold
 	//                        establishes existence of an element in the filter.
@@ -110,6 +122,9 @@ class CountingBloomFilter
 	size_t m_sizeInBytes;
 	unsigned m_hashNum;
 	unsigned m_kmerSize;
+	double m_dFPR; //unused
+	uint64_t m_nEntry; //unused
+	uint64_t m_tEntry; //unused
 	static const uint32_t BloomFilter_VERSION = 2;
 	unsigned m_countThreshold;
 	unsigned m_bitsPerCounter = 8;
@@ -258,7 +273,10 @@ CountingBloomFilter<T>::filtered_FPR(void) const
 // Serialization interface.
 template<typename T>
 CountingBloomFilter<T>::CountingBloomFilter(const string& path, unsigned countThreshold)
-	  : m_countThreshold(countThreshold)
+	  : m_dFPR(0)
+	  , m_nEntry(0)
+      , m_tEntry(0)
+	  , m_countThreshold(countThreshold)
 {	
 	readFilter(path);
 }
