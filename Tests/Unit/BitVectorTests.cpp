9ea86368ac7f9fd25f2ef62bab9457804e842577
Johnathan Wong <jowong@jowong05.phage.bcgsc.ca>
2019-07-15 16:31:01 -0700

    BitVectorTests.cpp: add tests for 8 bit filter

--- Tests/Unit/BitVectorTests.cpp
+++ Tests/Unit/BitVectorTests.cpp
@@ -34,12 +34,11 @@ TEST_CASE("test fixture", "[BitVector]")
 
 	// Create a 2 bits vector
 	const size_t filterSize = 1000000000;
-	const unsigned bitsPerCounter = 2;
+	unsigned bitsPerCounter = 2;
 
 	BitVector<uint8_t> filter_2bits(filterSize, bitsPerCounter);
 
 	/* Set up values 3,2,1 for element 0,1,LAST respectively */
-
 	for (int i = 0; i < 3; i++) {
 		filter_2bits.insert(0);
 	}
@@ -52,10 +51,11 @@ TEST_CASE("test fixture", "[BitVector]")
 	filter_2bits.insert(lastElement);
 
 	// Create a 4 bits vector
-	const unsigned bitsPerCounter = 4;
+	bitsPerCounter = 4;
 
 	BitVector<uint8_t> filter_4bits(filterSize, bitsPerCounter);
 
+	/* Set up values 15,2,1 for element 0,1,LAST respectively */
 	for (int i = 0; i < 15; i++) {
 		filter_4bits.insert(0);
 	}
@@ -66,6 +66,23 @@ TEST_CASE("test fixture", "[BitVector]")
 
 	long lastElement = (filterSize * 8 * sizeof(uint8_t) / bitsPerCounter ) - 1 ;
 	filter_4bits.insert(lastElement);
+
+	// Create a 8 bits vector
+	bitsPerCounter = 8;
+
+	BitVector<uint8_t> filter_8bits(filterSize, bitsPerCounter);
+
+	/* Set up values 255,2,1 for element 0,1,LAST respectively */
+	for (int i = 0; i < 255; i++) {
+		filter_8bits.insert(0);
+	}
+
+	for (int i = 0; i < 2; i++) {
+		filter_8bits.insert(1);
+	}
+
+	long lastElement = (filterSize * 8 * sizeof(uint8_t) / bitsPerCounter ) - 1 ;
+	filter_8bits.insert(lastElement);
 
 
 
@@ -83,18 +100,28 @@ TEST_CASE("test fixture", "[BitVector]")
         assert(filter_4bits[0] == 15);
         assert(filter_4bits[1] == 2);
         assert(filter_4bits[filter_4bits.size() - 1] == 1);
+
+		/* Query elements 0,1,LAST and check for values 15,2,1, repectively */
+        assert(filter_8bits[0] == 255);
+        assert(filter_8bits[1] == 2);
+        assert(filter_8bits[filter_8bits.size() - 1] == 1);
 	}
 
 	SECTION("query for no presence")
 	{
-		/* Query everyelements besides 0,1,LAST and check they are equal to 0 */
+		/* Query every elements besides 0,1,LAST and check they are equal to 0 */
         for (int i = 2; i < (filter_2bits.size() - 1); i++) {
             assert(filter_2bits[i] == 0);
         }
 
-		/* Query everyelements besides 0,1,LAST and check they are equal to 0 */
+		/* Query every elements besides 0,1,LAST and check they are equal to 0 */
         for (int i = 2; i < (filter_4bits.size() - 1); i++) {
             assert(filter_4bits[i] == 0);
+        }
+
+		/* Query every elements besides 0,1,LAST and check they are equal to 0 */
+        for (int i = 2; i < (filter_8bits.size() - 1); i++) {
+            assert(filter_8bits[i] == 0);
         }
 	}
 
@@ -117,5 +144,14 @@ TEST_CASE("test fixture", "[BitVector]")
         assert(filter_4bits.atomicIncrement(0) == false);
         /* check that vector value didn't cahnge */
         assert(filter_4bits[0] == 15);
+
+		/* Increment and check*/
+		filter_8bits.insert(2);
+        assert(filter_8bits[2] == 1);
+
+        /* check that vector doesn't allow overflow */
+        assert(filter_8bits.atomicIncrement(0) == false);
+        /* check that vector value didn't cahnge */
+        assert(filter_8bits[0] == 255);
 	}
 } /* end test fixture */
