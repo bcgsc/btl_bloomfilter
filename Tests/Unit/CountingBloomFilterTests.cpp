225bb9f4db6e5c157b91db379fca9168e2744b3a
Johnathan Wong <jowong@jowong05.phage.bcgsc.ca>
2019-07-16 10:57:25 -0700

    CountingBloomFilterTests.cpp: add more test to check size and 64 bit test

--- Tests/Unit/CountingBloomFilterTests.cpp
+++ Tests/Unit/CountingBloomFilterTests.cpp
@@ -66,7 +66,7 @@ TEST_CASE("test fixture", "[CountingBloomFilter]")
 
 	/* START COMMON SETUP CODE */
 
-	const size_t filterSize = 1000000000;
+	const size_t filterSize = 100000;
 	const unsigned numHashes = 5;
 	const unsigned threshold = 1;
 	const unsigned k = 4;
@@ -82,6 +82,16 @@ TEST_CASE("test fixture", "[CountingBloomFilter]")
 		++insertIt;
 	}
 
+	CountingBloomFilter<uint64_t> filter_64bit(filterSize, numHashes, k, threshold);
+
+	/* insert k-mers ACGT, CGTA, GTAC */
+
+	ntHashIterator insertIt2(seq, numHashes, k);
+	while (insertIt2 != insertIt2.end()) {
+		filter_64bit.insert(*insertIt2);
+		++insertIt2;
+	}
+
 	/* END COMMON SETUP CODE */
 
 	SECTION("query elements")
@@ -91,6 +101,7 @@ TEST_CASE("test fixture", "[CountingBloomFilter]")
 		ntHashIterator queryIt(seq, numHashes, k);
 		while (queryIt != queryIt.end()) {
 			assert(filter.contains(*queryIt));
+            assert(filter_64bit.contains(*queryIt));
 			++queryIt;
 		}
 
@@ -100,16 +111,78 @@ TEST_CASE("test fixture", "[CountingBloomFilter]")
 		ntHashIterator queryIt2(seq2, numHashes, k);
 		while (queryIt2 != queryIt2.end()) {
 			assert(!filter.contains(*queryIt2));
+			assert(!filter_64bit.contains(*queryIt2));
 			++queryIt2;
 		}
 	}
 
-	SECTION("save/load Bloom file")
+	SECTION("save/load 8 bit Bloom file")
 	{
 		/* write filter */
 
 		string filename = createTempFile();
 		filter.writeFilter(filename);
+		ifstream ifile(filename.c_str());
+
+		/* check size of newly-created file */
+
+		assert(ifile.is_open());
+
+		/* File header has no fixed element but "[HeaderEnd]"
+		   so check for "[HeaderEnd]" in file*/
+
+		std::string headerEnd = "[HeaderEnd]";
+		std::string line;
+		bool headerEndCheck = false;
+		while (std::getline(ifile, line)) {
+			if (line == headerEnd) {
+				headerEndCheck = true;
+				break;
+			}
+		}
+		assert(headerEndCheck);
+
+		size_t currPos = ifile.tellg();
+		ifile.seekg(0, ios::end);
+		size_t endPos = ifile.tellg();
+
+		// file size - header should be same as filter size rounded to multiple of 64
+
+        int remainder = filterSize % 64;
+		assert((endPos - currPos) == (filterSize + 64 - remainder));
+		ifile.close();
+
+		/* check loading of stored filter */
+
+		CountingBloomFilter<uint8_t> filter2(filename, threshold);
+
+        // Checking if sizeInBytes correspond to filesize-header
+
+        assert((endPos - currPos) == filter2.sizeInBytes());
+
+        // In a 8bit filter, size should be equal to size_in_bytes
+
+        assert(filter2.size() == filter2.sizeInBytes());
+
+		/* check if loaded filter is able to report expected results */
+
+		ntHashIterator queryIt(seq, numHashes, k);
+		while (queryIt != queryIt.end()) {
+			assert(filter2.contains(*queryIt));
+			++queryIt;
+		}
+
+		/* cleanup */
+
+		remove(filename.c_str());
+	}
+
+	SECTION("save/load 64 bit Bloom file")
+	{
+		/* write filter */
+
+		string filename = createTempFile();
+		filter_64bit.writeFilter(filename);
 		ifstream ifile(filename.c_str());
 
 		/* check size of newly-created file */
@@ -131,19 +204,31 @@ TEST_CASE("test fixture", "[CountingBloomFilter]")
 		size_t currPos = ifile.tellg();
 		ifile.seekg(0, ios::end);
 		size_t endPos = ifile.tellg();
-		// file size - header should be same as filter size
-		assert((endPos - currPos) == filterSize);
+		// file size - header should be same as filter size rounded to multiple of 64
+        int remainder = filterSize % 64;
+
+		assert((endPos - currPos) == (filterSize + 64 - remainder));
+
 		ifile.close();
 
 		/* check loading of stored filter */
 
-		CountingBloomFilter<uint8_t> filter2(filename, threshold);
+		CountingBloomFilter<uint64_t> filter_64bit2(filename, threshold);
+
+        // Checking if sizeInBytes correspond to filesize-header
+        
+        std::cerr << filter_64bit2.size() << std::endl;
+        assert((endPos - currPos) == filter_64bit2.sizeInBytes());
+
+        // In a 64bit filter, size * 8should be equal to size_in_bytes
+
+        assert((filter_64bit2.size() * 8) == filter_64bit2.sizeInBytes());
 
 		/* check if loaded filter is able to report expected results */
 
 		ntHashIterator queryIt(seq, numHashes, k);
 		while (queryIt != queryIt.end()) {
-			assert(filter2.contains(*queryIt));
+			assert(filter_64bit2.contains(*queryIt));
 			++queryIt;
 		}
 
