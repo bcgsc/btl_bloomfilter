ee8cc49b334d07165e94b80996351fdb3cc88a91
Johnathan Wong <jowong@jowong05.phage.bcgsc.ca>
2019-07-23 16:11:50 -0700

    BitVectorTests.cpp: typedef uint64_t as T

--- Tests/Unit/BitVectorTests.cpp
+++ Tests/Unit/BitVectorTests.cpp
@@ -10,11 +10,12 @@
 
 /* lightweight unit test framework */
 #include "BitVector.hpp"
-#include "catch.hpp"
+#include "vendor/catch.hpp"
 
 #include <assert.h>
 
 using namespace std;
+typedef uint64_t T;
 
 TEST_CASE("test fixture", "[BitVector]")
 {
@@ -36,7 +37,7 @@ TEST_CASE("test fixture", "[BitVector]")
 	const size_t filterSize = 1000000000;
 	unsigned bitsPerCounter = 2;
 
-	BitVector<uint8_t> filter_2bits(filterSize, bitsPerCounter);
+	BitVector filter_2bits(filterSize, bitsPerCounter);
 
 	/* Set up values 3,2,1 for element 0,1,LAST respectively */
 	for (int i = 0; i < 3; i++) {
@@ -47,13 +48,13 @@ TEST_CASE("test fixture", "[BitVector]")
 		filter_2bits.insert(1);
 	}
 
-	long lastElement = (filterSize * 8 * sizeof(uint8_t) / bitsPerCounter ) - 1 ;
+	long lastElement = (filterSize * 8 / sizeof(T) / bitsPerCounter) - 1;
 	filter_2bits.insert(lastElement);
 
 	// Create a 4 bits vector
 	bitsPerCounter = 4;
 
-	BitVector<uint8_t> filter_4bits(filterSize, bitsPerCounter);
+	BitVector filter_4bits(filterSize, bitsPerCounter);
 
 	/* Set up values 15,2,1 for element 0,1,LAST respectively */
 	for (int i = 0; i < 15; i++) {
@@ -64,13 +65,13 @@ TEST_CASE("test fixture", "[BitVector]")
 		filter_4bits.insert(1);
 	}
 
-	lastElement = (filterSize * 8 * sizeof(uint8_t) / bitsPerCounter ) - 1 ;
+	lastElement = (filterSize * 8 / sizeof(T) / bitsPerCounter) - 1;
 	filter_4bits.insert(lastElement);
 
 	// Create a 8 bits vector
 	bitsPerCounter = 8;
 
-	BitVector<uint8_t> filter_8bits(filterSize, bitsPerCounter);
+	BitVector filter_8bits(filterSize, bitsPerCounter);
 
 	/* Set up values 255,2,1 for element 0,1,LAST respectively */
 	for (int i = 0; i < 255; i++) {
@@ -81,77 +82,74 @@ TEST_CASE("test fixture", "[BitVector]")
 		filter_8bits.insert(1);
 	}
 
-	lastElement = (filterSize * 8 * sizeof(uint8_t) / bitsPerCounter ) - 1 ;
+	lastElement = (filterSize * 8 / sizeof(T) / bitsPerCounter) - 1;
 	filter_8bits.insert(lastElement);
-
-
-
 
 	/* END COMMON SETUP CODE */
 
 	SECTION("query elements")
 	{
 		/* Query elements 0,1,LAST and check for values 3,2,1, repectively */
-        assert(filter_2bits[0] == 3);
-        assert(filter_2bits[1] == 2);
-        assert(filter_2bits[filter_2bits.size() - 1] == 1);
+		assert(filter_2bits[0] == 3);
+		assert(filter_2bits[1] == 2);
+		assert(filter_2bits[filter_2bits.size() - 1] == 1);
 
 		/* Query elements 0,1,LAST and check for values 15,2,1, repectively */
-        assert(filter_4bits[0] == 15);
-        assert(filter_4bits[1] == 2);
-        assert(filter_4bits[filter_4bits.size() - 1] == 1);
+		assert(filter_4bits[0] == 15);
+		assert(filter_4bits[1] == 2);
+		assert(filter_4bits[filter_4bits.size() - 1] == 1);
 
 		/* Query elements 0,1,LAST and check for values 15,2,1, repectively */
-        assert(filter_8bits[0] == 255);
-        assert(filter_8bits[1] == 2);
-        assert(filter_8bits[filter_8bits.size() - 1] == 1);
+		assert(filter_8bits[0] == 255);
+		assert(filter_8bits[1] == 2);
+		assert(filter_8bits[filter_8bits.size() - 1] == 1);
 	}
 
 	SECTION("query for no presence")
 	{
 		/* Query every elements besides 0,1,LAST and check they are equal to 0 */
-        for (unsigned int i = 2; i < (filter_2bits.size() - 1); i++) {
-            assert(filter_2bits[i] == 0);
-        }
+		for (unsigned int i = 2; i < (filter_2bits.size() - 1); i++) {
+			assert(filter_2bits[i] == 0);
+		}
 
 		/* Query every elements besides 0,1,LAST and check they are equal to 0 */
-        for (unsigned int i = 2; i < (filter_4bits.size() - 1); i++) {
-            assert(filter_4bits[i] == 0);
-        }
+		for (unsigned int i = 2; i < (filter_4bits.size() - 1); i++) {
+			assert(filter_4bits[i] == 0);
+		}
 
 		/* Query every elements besides 0,1,LAST and check they are equal to 0 */
-        for (unsigned int i = 2; i < (filter_8bits.size() - 1); i++) {
-            assert(filter_8bits[i] == 0);
-        }
+		for (unsigned int i = 2; i < (filter_8bits.size() - 1); i++) {
+			assert(filter_8bits[i] == 0);
+		}
 	}
 
 	SECTION("insert elements and check")
 	{
 		/* Increment and check*/
 		filter_2bits.insert(2);
-        assert(filter_2bits[2] == 1);
+		assert(filter_2bits[2] == 1);
 
-        /* check that vector doesn't allow overflow */
-        assert(filter_2bits.atomicIncrement(0) == false);
-        /* check that vector value didn't cahnge */
-        assert(filter_2bits[0] == 3);
+		/* check that vector doesn't allow overflow */
+		assert(filter_2bits.atomicIncrement(0) == false);
+		/* check that vector value didn't cahnge */
+		assert(filter_2bits[0] == 3);
 
 		/* Increment and check*/
 		filter_4bits.insert(2);
-        assert(filter_4bits[2] == 1);
+		assert(filter_4bits[2] == 1);
 
-        /* check that vector doesn't allow overflow */
-        assert(filter_4bits.atomicIncrement(0) == false);
-        /* check that vector value didn't cahnge */
-        assert(filter_4bits[0] == 15);
+		/* check that vector doesn't allow overflow */
+		assert(filter_4bits.atomicIncrement(0) == false);
+		/* check that vector value didn't cahnge */
+		assert(filter_4bits[0] == 15);
 
 		/* Increment and check*/
 		filter_8bits.insert(2);
-        assert(filter_8bits[2] == 1);
+		assert(filter_8bits[2] == 1);
 
-        /* check that vector doesn't allow overflow */
-        assert(filter_8bits.atomicIncrement(0) == false);
-        /* check that vector value didn't cahnge */
-        assert(filter_8bits[0] == 255);
+		/* check that vector doesn't allow overflow */
+		assert(filter_8bits.atomicIncrement(0) == false);
+		/* check that vector value didn't cahnge */
+		assert(filter_8bits[0] == 255);
 	}
 } /* end test fixture */
