18846cdcfa6eec5ac3131b7982e43586e1a44bda
Johnathan Wong <jowong@jowong05.phage.bcgsc.ca>
2019-07-25 15:48:05 -0700

    Bin.hpp: use bitwise operations and remove m_value

--- Bin.hpp
+++ Bin.hpp
@@ -19,25 +19,17 @@ class Bin
 	  , m_bitsPerBin(bitsPerBin)
 	  , m_binNumber(binNumber)
 	  , m_bitmask(bitmask)
-	{
-		m_value = (m_word >> (m_binNumber * m_bitsPerBin)) & m_bitmask;
-	}
+	{}
 	T maxValue() const { return m_bitmask; };
-	operator uint64_t() const { return m_value; };
+	operator uint64_t() const { return (m_word >> (m_binNumber * m_bitsPerBin)) & m_bitmask; };
 	Bin& operator=(const T newVal)
 	{
 		if (newVal > maxValue()) {
 			std::cerr << "error: cannot set bin to values larger than " << maxValue() << "."
 			          << std::endl;
 		}
-		if (newVal > m_value) {
-			m_word = m_word + ((newVal - m_value) << (m_binNumber * m_bitsPerBin));
-		} else if (newVal < m_value) {
-			m_word = m_word - ((m_value - newVal) << (m_binNumber * m_bitsPerBin));
-		} else {
-			return *this;
-		}
-		m_value = newVal;
+		T oldValMask = (maxValue() << (m_binNumber * m_bitsPerBin)) ^ ((T)0 - 1);
+		m_word = (m_word & oldValMask) + (newVal << (m_binNumber * m_bitsPerBin));
 		return *this; // returns this for chaining
 	};
 	Bin& operator=(const Bin& bin);
@@ -51,8 +43,6 @@ class Bin
 	unsigned m_binNumber = 0;
 	/** Masking bit used in bit operations. E.g. 0b 0000 0011 */
 	T m_bitmask = 0;
-	/** Value of this bin */
-	T m_value = 0;
 };
 
 Bin&
@@ -67,14 +57,9 @@ Bin::operator=(const Bin& bin)
 		exit(EXIT_FAILURE);
 	}
 
-	if (bin.m_value > m_value) {
-		m_word = m_word + ((bin.m_value - m_value) << (m_binNumber * m_bitsPerBin));
-	} else if (bin.m_value < m_value) {
-		m_word = m_word - ((m_value - bin.m_value) << (m_binNumber * m_bitsPerBin));
-	} else {
-		return *this;
-	}
-	m_value = bin.m_value;
+	T newVal = (bin.m_word >> (bin.m_binNumber * bin.m_bitsPerBin)) & bin.m_bitmask;
+	T oldValMask = (maxValue() << (m_binNumber * m_bitsPerBin)) ^ ((T)0 - 1);
+	m_word = (m_word & oldValMask) + (newVal << (m_binNumber * m_bitsPerBin));
 
 	// return the existing object so we can chain this operator
 	return *this;
